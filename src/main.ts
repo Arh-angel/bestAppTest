861ccf979286befa17daf3753f06b1adc04b51be
AlexandrKh <khalex8993@gmail.com>
2023-11-01 16:51:09 +0300

    CHANGE: add log start server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = new ConfigService().get('PORT') || 9000;
+  await app.listen(port, () => console.log(`Server start in port: ${port}`));
 }
 bootstrap();
