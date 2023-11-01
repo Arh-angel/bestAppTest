0bfc1136639376e493b6e90a0b53f782ad2ac496
AlexandrKh <khalex8993@gmail.com>
2023-11-01 16:53:43 +0300

    CHANGE: add imports typeorm, modules and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './common/configs/typeorm.config';
+import { UsersModule } from './modules/users/users.module';
+import { ChatsModule } from './modules/chats/chats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRootAsync(TypeOrmConfigService()),
+    UsersModule,
+    ChatsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
